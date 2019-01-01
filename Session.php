02ad6385771113429eaf3223e9b9fe9ff4146196
Abraão Zaidan <abraao@cakeweb.com.br>
2019-01-01 17:42:08 -0200

    Unnecessary exception removed

--- Session.php
+++ Session.php
@@ -460,8 +460,8 @@ class Zend_Session extends Zend_Session_Abstract
         // See http://www.php.net/manual/en/ref.session.php for explanation
         if (!self::$_unitTestEnabled && defined('SID')) {
             /** @see Zend_Session_Exception */
-            require_once 'Zend/Session/Exception.php';
-            throw new Zend_Session_Exception('session has already been started by session.auto-start or session_start()');
+            // require_once 'Zend/Session/Exception.php';
+            // throw new Zend_Session_Exception('session has already been started by session.auto-start or session_start()');
         }
 
         /**
